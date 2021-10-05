d56c3d6861337832c78b738bbc03fe397e0ed0f3
egemensahiin <egemensahin95@gmail.com>
2021-10-05 19:20:28 +0300

    Orginazed groups.

--- config.h
+++ config.h
@@ -33,20 +33,21 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class         			 instance    title       		tags mask     isfloating   monitor */
-	{ "Gimp",        			  NULL,       NULL,     	 	1 << 4,       0,           -1 },
 	{ "Pavucontrol", 			  NULL,       NULL,     	 	0,            1,           -1 },
 	{ "Galculator", 			  NULL,       NULL,     	 	0,            1,           -1 },
 	{ "Firefox",     			  NULL,       NULL,     	 	1 << 1,       0,           -1 },
 	{ "qutebrowser", 			  NULL,       NULL,     	 	1 << 1,       0,           -1 },
+	{ "VSCodium",     			  NULL,       NULL,     	 	1 << 2,       0,           -1 },
+	{ NULL,  					  NULL,       "spt",			1 << 3,       0,           -1 },
+	{ "Gimp",        			  NULL,       NULL,     	 	1 << 4,       0,           -1 },
+	{ "Mpv",	     			  NULL,       NULL,     	 	1 << 4,       0,           -1 },
+	{ "libreoffice-startcenter",  NULL,       NULL,				1 << 5,       0,           -1 },
+	{ "libreoffice-startcenter",  NULL,       NULL,				1 << 5,       0,           -1 },
 	{ "discord",     			  NULL,       NULL,     	 	1 << 6,       0,           -1 },
 	{ "PyMOL",	     			  NULL,       NULL,     	 	1 << 7,       0,           -1 },
-	{ "Mpv",	     			  NULL,       NULL,     	 	1 << 4,       0,           -1 },
-	{ "VSCodium",     			  NULL,       NULL,     	 	1 << 2,       0,           -1 },
-	{ "VirtualBox Manager", 	  NULL,       NULL,				1 << 8,       0,           -1 },
-	{ "libreoffice-startcenter",  NULL,       NULL,				1 << 5,       0,           -1 },
-	{ "libreoffice-startcenter",  NULL,       NULL,				1 << 5,       0,           -1 },
-	{ NULL,  					  NULL,       "spt",			1 << 3,       0,           -1 },
 	{ NULL,  					  NULL,       "AutoDockTools",	1 << 7,       0,           -1 },
+	{ "VirtualBox Manager", 	  NULL,       NULL,				1 << 8,       0,           -1 },
+	{ "DiscoveryStudio2021-bin",  NULL,       NULL,				1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
